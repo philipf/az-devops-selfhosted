e2ebab83f0b48e5ef443b44b0af37fdca72a9083
Philip Fourie <philip.fourie@theta.co.nz>
2021-04-25 14:23:35 +1200

    Simple Greet  function added

---
+++ SampleFunc/Greet.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SampleFunc
+{
+    public static class Greet
+    {
+        [FunctionName("Greet")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string version = "1.0";
+            return new OkObjectResult($"Hello World: {version} - {DateTime.Now.ToString()}");
+        }
+    }
+}
